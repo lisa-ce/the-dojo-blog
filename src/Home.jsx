53fd98d0983ad1a8d47ba1a9f14f208cc8862e8b
Lisa Chikovore <lisachikovore@gmail.com>
2026-07-05 14:31:29 +0200

    added blogs

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,36 @@
+import React from 'react'
+
 const Home = () => {
+
+
+// let name = 'Mario';
+// const [name, setName] = React.useState('Mario');
+// const [age, setAge] = React.useState(25);
+
+//   const handleClick = () => {
+// setName('Luigi');
+// setAge(30);
+//   } 
+
+
+const [blogs, setBlogs] = React.useState([
+  { title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1 },
+  { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
+  { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 }
+]);
   return ( 
+
     <div className="home">
-      <h2>Home Page</h2>
-    </div>
+    {blogs.map((blog)=>(
+      <div className="blog-preview" key={blog.id}>
+        <h2>{blog.title}</h2>
+        <p>Written by {blog.author}</p>
+      </div>
+    ))}
+     </div>
+
+
+
    );
 }
  
