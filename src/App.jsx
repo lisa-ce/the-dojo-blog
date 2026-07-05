efdf307be988b7678bc133ee78cadcbde02a874d
Lisa Chikovore <lisachikovore@gmail.com>
2026-07-05 16:30:03 +0200

    added routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import Navbar from './Navbar.jsx'
-import Home from './Home.jsx'
-
+import React from "react";
+import Navbar from "./Navbar.jsx";
+import Home from "./Home.jsx";
+import Create from "./Create.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BlogDetails from "./BlogDetails.jsx";  
 
 function App() {
-  
-// const title="Welcome to the Dojo Blog"
-// const likes=50
-
   return (
-    <>
-      
-        <div className="App">
+    <Router>
+      <div className="App">
         <Navbar />
+
         <div className="content">
-        <Home />
-      
-         </div>
-         </div>
-    </> 
-  )
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route exact path="/create">
+              <Create />
+            </Route>
+
+
+
+            <Route exact path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+
+
+
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
